95d11cd2fcdb042d65c8f5b9fc44aab68e250532
Sotos2013 <www.sotihatzi@gmail.com>
2025-05-29 13:58:41 +0300

    change for gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.untitled1"
+    namespace = "gr.ihu.techmuseum"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -28,7 +28,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.untitled1"
+        applicationId = "gr.ihu.techmuseum"
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
--- android/app/src/main/kotlin/gr/ihu/techmuseum/MainActivity.kt
+++ android/app/src/main/kotlin/gr/ihu/techmuseum/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.untitled1
+package gr.ihu.techmuseum
 
 import io.flutter.embedding.android.FlutterActivity
 
